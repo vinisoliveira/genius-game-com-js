02104836b0e5c402f032c0308c06de5885f9f338
Vinícius Oliveira <vinicius.oliveira1911@gmail.com>
2022-05-04 18:15:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -15,6 +15,34 @@ const yellow = document.querySelector('.yellow');
 let shuffleOrder = () => {
     let colorOrder = Math.floor(Math.random() * 4 )
     order[order.length] = colorOrder;
-    clickOrder = [];
+    clickedOrder = [];
 
+    for (let i in order) {
+        let elementColor = createElement(order[i]);
+        lightColor(elementColor, Number[i] + 1)
+    }
+
+}
+
+let lightColor = (element, number) => {
+    number = number * 500;
+    setTimeout(() => {
+        element.classList.add('selected');
+    }, number - 250);
+    setTimeout(() => {
+        element.classList.remove('selected');
+    });
+}
+
+let checkOrder = () => {
+    for(let i in clickedOrder) {
+        if (clickedOrder[i] != order[i]) {
+            lose();
+            break;
+        }
+    }
+    if(clickedOrder.length == order.length) {
+        alert('Pontuação: ${score}\nVocê acertou! Iniciando próximo nível!');
+        nextLevel()
+    }
 }
